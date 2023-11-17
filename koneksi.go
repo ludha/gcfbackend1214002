815b108d86e461fb7fd2bf7ced900de9fe6d775c
ludha <1214002@std.ulbi.ac.id>
2023-11-17 10:07:25 +0700

    push

---
+++ koneksi.go
@@ -0,0 +1,70 @@
+package gcfbackend1214002
+
+import (
+	"context"
+	"os"
+
+	"github.com/aiteung/atdb"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func SetConnection(MONGOCONNSTRINGENV, dbname string) *mongo.Database {
+	var DBmongoinfo = atdb.DBInfo{
+		DBString: os.Getenv(MONGOCONNSTRINGENV),
+		DBName:   dbname,
+	}
+	return atdb.MongoConnect(DBmongoinfo)
+}
+
+func GetAllBangunanLineString(mongoconn *mongo.Database, collection string) []GeoJson {
+	lokasi := atdb.GetAllDoc[[]GeoJson](mongoconn, collection)
+	return lokasi
+}
+
+func IsPasswordValid(mongoconn *mongo.Database, collection string, userdata User) bool {
+	filter := bson.M{"username": userdata.Username}
+	res := atdb.GetOneDoc[User](mongoconn, collection, filter)
+	return CheckPasswordHash(userdata.Password, res.Password)
+}
+
+func InsertDataLonlat(mongoconn *mongo.Database, collection string, coordinate []float64, name, volume, tipe string) (InsertedID interface{}) {
+	req := new(CoorLonLatProperties)
+	req.Type = tipe
+	req.Coordinates = coordinate
+	req.Name = name
+	req.Volume = volume
+
+	ins := atdb.InsertOneDoc(mongoconn, collection, req)
+	return ins
+}
+
+func UpdateDataGeojson(mongoconn *mongo.Database, colname, name, newVolume, newTipe string) error {
+	// Filter berdasarkan nama
+	filter := bson.M{"name": name}
+
+	// Update data yang akan diubah
+	update := bson.M{
+		"$set": bson.M{
+			"volume": newVolume,
+			"tipe":   newTipe,
+		},
+	}
+
+	// Mencoba untuk mengupdate dokumen
+	_, err := mongoconn.Collection(colname).UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func DeleteDataGeojson(mongoconn *mongo.Database, colname string, name string) (*mongo.DeleteResult, error) {
+	filter := bson.M{"name": name}
+	del, err := mongoconn.Collection(colname).DeleteOne(context.TODO(), filter)
+	if err != nil {
+		return nil, err
+	}
+	return del, nil
+}
---
+++ ludha.go
@@ -0,0 +1,163 @@
+package gcfbackend1214002
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	pasproj "github.com/e-dumas-sukasari/webpasetobackend"
+	"github.com/petapedia/peda"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/whatsauth/watoken"
+)
+
+func GCFHandler(MONGOCONNSTRINGENV, dbname, collectionname string) string {
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	datagedung := GetAllBangunanLineString(mconn, collectionname)
+	return GCFReturnStruct(datagedung)
+}
+
+func GCFPostCoordinateLonLat(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	req := new(Credential)
+	conn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	resp := new(CoorLonLatProperties)
+	err := json.NewDecoder(r.Body).Decode(&resp)
+	if err != nil {
+		req.Status = false
+		req.Message = "error parsing application/json: " + err.Error()
+	} else {
+		req.Status = true
+		Ins := InsertDataLonlat(conn, collectionname,
+			resp.Coordinates,
+			resp.Name,
+			resp.Volume,
+			resp.Type)
+		req.Message = fmt.Sprintf("%v:%v", "Berhasil Input data", Ins)
+	}
+	return GCFReturnStruct(req)
+}
+
+func GCFPostHandler(PASETOPRIVATEKEYENV, MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	var Response Credential
+	Response.Status = false
+	mconn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	var datauser User
+	err := json.NewDecoder(r.Body).Decode(&datauser)
+	if err != nil {
+		Response.Message = "error parsing application/json: " + err.Error()
+	} else {
+		if IsPasswordValid(mconn, collectionname, datauser) {
+			Response.Status = true
+			tokenstring, err := watoken.Encode(datauser.Username, os.Getenv(PASETOPRIVATEKEYENV))
+			if err != nil {
+				Response.Message = "Gagal Encode Token : " + err.Error()
+			} else {
+				Response.Message = "Selamat Datang"
+				Response.Token = tokenstring
+			}
+		} else {
+			Response.Message = "Password Salah"
+		}
+	}
+
+	return GCFReturnStruct(Response)
+}
+
+func SignUpGCF(MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	resp := new(pasproj.Credential)
+	userdata := new(RegistUser)
+	resp.Status = false
+	conn := SetConnection(MONGOCONNSTRINGENV, dbname)
+	err := json.NewDecoder(r.Body).Decode(&userdata)
+	if err != nil {
+		resp.Message = "error parsing application/json: " + err.Error()
+	} else {
+		resp.Status = true
+		hash, err := pasproj.HashPass(userdata.Password)
+		if err != nil {
+			resp.Message = "Gagal Hash Password" + err.Error()
+		}
+		InsertUserdata(conn, userdata.Username, hash)
+		resp.Message = "Berhasil Input data"
+	}
+	response := pasproj.ReturnStringStruct(resp)
+	return response
+}
+
+func SignInGCF(PASETOPRIVATEKEYENV, MONGOCONNSTRINGENV, dbname, collectionname string, r *http.Request) string {
+	var resp pasproj.Credential
+	mconn := pasproj.MongoCreateConnection(MONGOCONNSTRINGENV, dbname)
+	var datauser peda.User
+	err := json.NewDecoder(r.Body).Decode(&datauser)
+	if err != nil {
+		resp.Message = "error parsing application/json: " + err.Error()
+	} else {
+		if peda.IsPasswordValid(mconn, collectionname, datauser) {
+			tokenstring, err := watoken.Encode(datauser.Username, os.Getenv(PASETOPRIVATEKEYENV))
+			if err != nil {
+				resp.Message = "Gagal Encode Token : " + err.Error()
+			} else {
+				resp.Status = true
+				resp.Message = "Selamat Datang"
+				resp.Token = tokenstring
+			}
+		} else {
+			resp.Message = "Password Salah"
+		}
+	}
+	return pasproj.ReturnStringStruct(resp)
+}
+
+func GCFUpdateGeo(Mongostring, dbname, colname string, r *http.Request) string {
+	req := new(Credential)
+	resp := new(CoorLonLatProperties)
+	conn := SetConnection(Mongostring, dbname)
+	err := json.NewDecoder(r.Body).Decode(&resp)
+	if err != nil {
+		req.Status = false
+		req.Message = "error parsing application/json: " + err.Error()
+	} else {
+		req.Status = true
+		Ins := UpdateDataGeojson(conn, colname,
+			resp.Name,
+			resp.Volume,
+			resp.Type)
+		req.Message = fmt.Sprintf("%v:%v", "Berhasil Update data", Ins)
+	}
+	return GCFReturnStruct(req)
+}
+
+func GCFDelDataGeo(Mongostring, dbname, colname string, r *http.Request) string {
+	req := new(Credential)
+	resp := new(CoorLonLatProperties)
+	conn := SetConnection(Mongostring, dbname)
+	err := json.NewDecoder(r.Body).Decode(&resp)
+	if err != nil {
+		req.Status = false
+		req.Message = "error parsing application/json: " + err.Error()
+	} else {
+		req.Status = true
+		delResult, delErr := DeleteDataGeojson(conn, colname, resp.Name)
+		if delErr != nil {
+			req.Status = false
+			req.Message = "error deleting data: " + delErr.Error()
+		} else {
+			req.Message = fmt.Sprintf("Berhasil menghapus data. Jumlah data terhapus: %v", delResult.DeletedCount)
+		}
+	}
+	return GCFReturnStruct(req)
+}
+
+func GCFReturnStruct(DataStuct any) string {
+	jsondata, _ := json.Marshal(DataStuct)
+	return string(jsondata)
+}
+
+func InsertUserdata(MongoConn *mongo.Database, username, password string) (InsertedID interface{}) {
+	req := new(User)
+	req.Username = username
+	req.Password = password
+	return pasproj.InsertOneDoc(MongoConn, "user", req)
+}
